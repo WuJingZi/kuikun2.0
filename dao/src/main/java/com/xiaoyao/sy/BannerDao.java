f1b6cc3815137c960948bf3505fb53525fd894d4
17783 <1778309352@qq.com>
2018-08-18 14:10:47 +0800

    增加banner

--- dao/src/main/java/com/xiaoyao/sy/BannerDao.java
+++ dao/src/main/java/com/xiaoyao/sy/BannerDao.java
@@ -3,10 +3,12 @@ package com.xiaoyao.sy;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.util.List;
+
 public interface BannerDao extends JpaRepository<Banner,String>,JpaSpecificationExecutor<Banner>{
 
 
-
+	List<Banner> findTop4ByItype(Integer itype);
 
 
 
